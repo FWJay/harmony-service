1a7035145cc17665fe47fe68af28b44c52d83dfd
Matt Brooke-Smith <matt@futureworkshops.com>
2016-07-02 16:56:17 +0100

    Update RpcService namespace

--- lib/harmony/service/rpc_service.rb
+++ lib/harmony/service/rpc_service.rb
@@ -15,46 +15,49 @@ opts = {
 Sneakers.configure(opts)
 Sneakers.logger.level = Logger::INFO
 
-class Harmony::Service::RpcService
+module Harmony
+  module Service
+    class RpcService
   
-  include Sneakers::Worker
-  from_queue ENV['harmony_queue']
+      include Sneakers::Worker
+      from_queue ENV['harmony_queue']
   
-  def work_with_params(message, delivery_info, metadata)
-    params = JSON.parse(message)   
-    result = work_with_message_params(params)
-    send_response(result.to_json, metadata.reply_to, metadata.correlation_id)
-    ack!
+      def work_with_params(message, delivery_info, metadata)
+        params = JSON.parse(message)   
+        result = work_with_message_params(params)
+        send_response(result.to_json, metadata.reply_to, metadata.correlation_id)
+        ack!
+      end
+  
+      def stop
+        super
+        reply_to_exchange.close
+        reply_to_connection.close
+      end
+  
+      def reply_to_connection
+        @reply_to_connection ||= create_reply_to_connection
+      end
+  
+      def create_reply_to_connection
+        opts = Sneakers::CONFIG
+        conn = Bunny.new(opts[:amqp], :vhost => opts[:vhost], :heartbeat => opts[:heartbeat], :logger => Sneakers::logger)
+        conn.start
+        conn
+      end
+  
+      def reply_to_exchange
+        @reply_to_queue ||= create_reply_to_exchange
+      end
+  
+      def create_reply_to_exchange
+        ch = reply_to_connection.create_channel
+        ch.default_exchange    
+      end
+  
+      def send_response(result, reply_to, correlation_id)
+        reply_to_exchange.publish(result, :routing_key => reply_to, :correlation_id => correlation_id)
+      end
+    end
   end
-  
-  def stop
-    super
-    reply_to_exchange.close
-    reply_to_connection.close
-  end
-  
-  def reply_to_connection
-    @reply_to_connection ||= create_reply_to_connection
-  end
-  
-  def create_reply_to_connection
-    opts = Sneakers::CONFIG
-    conn = Bunny.new(opts[:amqp], :vhost => opts[:vhost], :heartbeat => opts[:heartbeat], :logger => Sneakers::logger)
-    conn.start
-    conn
-  end
-  
-  def reply_to_exchange
-    @reply_to_queue ||= create_reply_to_exchange
-  end
-  
-  def create_reply_to_exchange
-    ch = reply_to_connection.create_channel
-    ch.default_exchange    
-  end
-  
-  def send_response(result, reply_to, correlation_id)
-    reply_to_exchange.publish(result, :routing_key => reply_to, :correlation_id => correlation_id)
-  end
-  
 end
