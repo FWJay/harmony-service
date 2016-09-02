038f7288b3d9f0fc148b616c57d90e83c913cfc7
Matt Brooke-Smith <matt@futureworkshops.com>
2016-09-02 10:28:23 +0100

    Update logging

--- lib/harmony/service/rpc_service.rb
+++ lib/harmony/service/rpc_service.rb
@@ -23,9 +23,10 @@ module Harmony
   
       def work_with_params(message, delivery_info, metadata)
         params = JSON.parse(message)   
+        logger.info "Request: #{params}"
         result = work_with_message_params(params)
-        logger.info result
         json = result.to_json
+        logger.info "Result: #{json}"
         send_response(json, metadata.reply_to, metadata.correlation_id)
         ack!
       end
