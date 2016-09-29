7a5e5a7312f2fbc0089631d8215c66b3c4f9081f
Matt Brooke-Smith <matt@futureworkshops.com>
2016-09-29 20:01:00 +0100

    specs

--- spec/harmony/rpc_service_spec.rb
+++ spec/harmony/rpc_service_spec.rb
@@ -2,31 +2,57 @@ require 'spec_helper'
 
 describe Harmony::Service::RpcService do
   
-  context "success" do
-    before(:each) do
-      allow(subject).to receive(:work_with_message_params) { {"success" => true} }
-      allow(subject).to receive(:send_response)
-      allow(subject).to receive(:ack!)
+  describe "#work_with_params" do
+    context "with json" do
+      before(:each) do
+        allow(subject).to receive(:work_with_request) { {"success" => true} }
+        allow(subject).to receive(:send_response)
+        allow(subject).to receive(:ack!)
             
-      metadata = instance_double("Metadata", reply_to: "harmony.trello", correlation_id: "abc123")
-      subject.work_with_params("{\"trello_board_id\": \"12345\"}", {}, metadata)
+        metadata = instance_double("Metadata", reply_to: "harmony.trello", correlation_id: "abc123")
+        subject.work_with_params("{\"trello_board_id\": \"12345\"}", {}, metadata)
+      end
+    
+      it { expect(subject).to have_received(:send_response).with("{\"success\":true}", "harmony.trello", "abc123") }
+      it { expect(subject).to have_received(:ack!) }
     end
     
-    it { expect(subject).to have_received(:send_response).with("{\"success\":true}", "harmony.trello", "abc123") }
-    it { expect(subject).to have_received(:ack!) }
+    context "with calculation request" do
+      let(:request) do
+        r = Harmony::Service::Message::Request::Calculation.new
+        r.harmony_user_email = "matt@futureworkshops.com"
+        r.inputs = {"lat" => "51.0", "lon" => "0.1"}
+        r
+      end
+      
+      before(:each) do
+        allow(subject).to receive(:work_with_request) { {"success" => true} }
+        allow(subject).to receive(:send_response)
+        allow(subject).to receive(:ack!)
+            
+        metadata = instance_double("Metadata", reply_to: "harmony.trello", correlation_id: "abc123")
+        subject.work_with_params(Oj.dump(request), {}, metadata)
+      end
+
+      it { expect(subject).to have_received(:work_with_request).with(kind_of(Harmony::Service::Message::Request::Calculation)) }    
+      it { expect(subject).to have_received(:send_response).with("{\"success\":true}", "harmony.trello", "abc123") }
+      it { expect(subject).to have_received(:ack!) }
+    end
+  
+    context "failure" do
+      before(:each) do
+        allow(subject).to receive(:work_with_request).and_raise("A timeout occured")
+        allow(subject).to receive(:send_response)
+        allow(subject).to receive(:reject!)
+            
+        metadata = instance_double("Metadata", reply_to: "harmony.trello", correlation_id: "abc123")
+        subject.work_with_params("{\"trello_board_id\": \"12345\"}", {}, metadata)
+      end
+    
+      it { expect(subject).to have_received(:send_response).with("{\"success\":false,\"message\":\"An error occured\",\"detailed_message\":\"A timeout occured\"}", "harmony.trello", "abc123") }
+      it { expect(subject).to have_received(:reject!) }
+    end
   end
   
-  context "failure" do
-    before(:each) do
-      allow(subject).to receive(:work_with_message_params).and_raise("A timeout occured")
-      allow(subject).to receive(:send_response)
-      allow(subject).to receive(:reject!)
-            
-      metadata = instance_double("Metadata", reply_to: "harmony.trello", correlation_id: "abc123")
-      subject.work_with_params("{\"trello_board_id\": \"12345\"}", {}, metadata)
-    end
-    
-    it { expect(subject).to have_received(:send_response).with("{\"success\":false,\"message\":\"An error occured\",\"detailed_message\":\"A timeout occured\"}", "harmony.trello", "abc123") }
-    it { expect(subject).to have_received(:reject!) }
-  end
+
 end
