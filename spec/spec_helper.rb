7b75818ced296a582748d4959398e8a39cbdc32c
Matt Brooke-Smith <matt@futureworkshops.com>
2017-01-17 22:41:29 +0000

    spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,24 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'harmony/service'
 require 'byebug'
+
+opts = {
+  amqp: 'amqp://localhost:5672',
+  vhost: '/',
+  exchange: 'sneakers',
+  exchange_type: :direct,
+  metrics: Sneakers::Metrics::LoggingMetrics.new,
+  handler: Sneakers::Handlers::Maxretry,
+  handler_class: "MockHandler"
+}
+
+Sneakers.configure(opts)
+Sneakers.logger.level = Logger::DEBUG
+
+class MockHandler
+  
+  def work_with_request(request)
+    
+  end
+  
+end
