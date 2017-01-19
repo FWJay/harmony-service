0e8ba94acae6da0657d889c7fcf2cfc500cac0cc
Matt Brooke-Smith <matt@futureworkshops.com>
2017-01-19 22:43:45 +0000

    Update chart request
    Add properties

--- lib/harmony/service/chart/request.rb
+++ lib/harmony/service/chart/request.rb
@@ -1,3 +1,4 @@
 class Harmony::Service::Chart::Request < Harmony::Service::Request
-
+  attr_accessor :uri # for source files
+  attr_accessor :output_range # range inside source file (i.e. excel)
 end
