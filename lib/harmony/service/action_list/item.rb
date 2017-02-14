6f112068d75544a10e17b575ea8f570e7d38eaf5
fw.matt <matt@futureworkshops.com>
2017-02-14 12:25:25 +0400

    Update Harmony::Service::ActionList::Item
    Remove detail_url

--- lib/harmony/service/action_list/item.rb
+++ lib/harmony/service/action_list/item.rb
@@ -1,3 +1,3 @@
 class Harmony::Service::ActionList::Item < Harmony::Service::Response
-  attr_accessor :id, :title, :subtitle, :actions, :detail_url, :detail_html
+  attr_accessor :id, :title, :subtitle, :actions, :detail_html
 end
